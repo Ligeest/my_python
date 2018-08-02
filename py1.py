413653cf534a227d6667fcc90830d974ad6f693f
Ligeest <30897514+Ligeest@users.noreply.github.com>
2018-08-02 13:37:26 +0800

    Update py1.py

--- py1.py
+++ py1.py
@@ -1,25 +1,24 @@
-print('ssssssssssssss')
 import re
 import urllib.request
 
 page = 2
-url = 'http://www.qiushibaike.com/8hr/page/'+str(page)+'/'
-headers = ('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.163 Safari/535.1')
+url = 'https://www.qiushibaike.com/8hr/page/'+str(page)+'/'
+headers = ('User-Agent','Mozilla/5.0 (Windows NT 12.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.1453.94 Safari/537.36')
 opener = urllib.request.build_opener()
-#opener.addheaders = [headers]
+opener.addheaders = [headers]
 urllib.request.install_opener(opener)
-#data = urllib.request.urlopen(url).read().decode('utf-8')
-data = urllib.request.urlopen(url).read()
-print(data)
+data = urllib.request.urlopen(url).read().decode('utf-8')
+#data = urllib.request.urlopen(url).read()
 userpat = 'target="_blank"title="(.*?)">'
 contentpat = '<div class="content">(.*?)</div>'
-serlist = re.compile(userpat,re.S).findall(data)
+userlist = re.compile(userpat,re.S).findall(data)
 contentlist = re.compile(contentpat,re.S).findall(data)
 x = 1
 for content in contentlist:
     content = content.replace("╲n","")
     name = "content" + str(x)
-    exec(name+'=content')
+    #exec(name+'=content')
+    print(content)
     x += 1
 y = 1
 for user in userlist:
@@ -27,14 +26,7 @@ for user in userlist:
     #with open(user,'w') as f:
         #f.write()
     print('user:'+str(page)+user)
-    print('sfg43y34sb2452')
+    print('内容是:')
     exec("print("+name+")")
     print("\n")
     y += 1
-
-
-
-
-
-        
-
