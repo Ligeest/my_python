6067c4aa29ba42b7bede3727c09995d1c4865488
Ligeest <30897514+Ligeest@users.noreply.github.com>
2018-08-17 15:33:39 +0800

    Add files via upload

--- img.py
+++ img.py
@@ -10,17 +10,19 @@ def file_name(file_dir):
     for files in os.walk(file_dir):  
          return(files) 
 
-imgs = file_name("E:\\文件\\python\\imgs")
-#print(imgs)
-for img in imgs[2:]:
-    print("")
-for img1 in img:
-    if file_extension(img1) == '.py':
-        print(file_extension(img1))
-    else:
-        img_info = Image.open(img1)  
+file_dir = 'C:\\Users\\17703\\Desktop\\下载\\'
+imgs = file_name("C:\\Users\\17703\\Desktop\\下载")
+imgs = imgs[2:]
+img_list = []
+for img in imgs[0]:
+    if file_extension(file_dir+img) == '.jpg':
+        img_info = Image.open(file_dir+img)  
         size = img_info.size
         length = max(size)
         width = min(size)
         if length == width:
-            shutil.copy('E:\\文件\\python\\imgs\\' + img1,'E:\\文件\\头像\\1')
+            print(file_dir+img)
+            img_list.append(file_dir+img)
+            shutil.copy(file_dir+img,'E:\\tx\\')
+for i in range(len(img_list)):
+    os.remove(img_list[i])
